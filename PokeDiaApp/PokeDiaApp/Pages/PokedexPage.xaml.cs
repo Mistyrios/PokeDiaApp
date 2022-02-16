05302b29325162dca0f9f4df64037e6cb28aa22b
Thibault Dufour <thibault.dufour@etu.unilim.fr>
2022-02-16 14:20:25 +0100

    [Feat] Ajout viewModel

--- PokeDiaApp/PokeDiaApp/Pages/PokedexPage.xaml.cs
+++ PokeDiaApp/PokeDiaApp/Pages/PokedexPage.xaml.cs
@@ -8,27 +8,11 @@ namespace PokeDiaApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PokedexPage : ContentPage
     {
-        private ObservableCollection<Pokemons> myList;
-
-        private ObservableCollection<Pokemons> MyList
-        {
-            get { return myList; }
-            set { myList = value; }
-        }
+       
 
         public PokedexPage()
         {
-            InitializeComponent();
-            BindingContext = this;
-
-            MyList = new ObservableCollection<Pokemons>();
-
-            for (int i = 1; i < 10; i++)
-            {
-                MyList.Add(new Pokemons() { Number = i, Name = "piplup" + i.ToString(), Type1 = "water" + i.ToString(), Type2 = "fire" + i.ToString(),Url1 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/393.png" });
-            }
-
-            pokemonsList.ItemsSource = MyList;
+           
         }
 
         async void ShowDetails(object sender, EventArgs args)
--- PokeDiaApp/PokeDiaApp/ViewModel/ListeViewModel.cs
+++ PokeDiaApp/PokeDiaApp/ViewModel/ListeViewModel.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 
 namespace PokeDiaApp.ViewModel
 {
-    internal class ListeViewModel
+    internal class ListeViewModel : BaseViewModel
     {
+        private static ListeViewModel _instance = new ListeViewModel();
+        public static ListeViewModel Instance { get { return _instance; } }
+
+        public ObservableCollection<Pokemons> MyList {
+                    get { return myList; }
+                    set { myList = value; }
+        }
+ 
+        private ObservableCollection<Pokemons> myList;
+
+        public ListeViewModel()
+        {
+             MyList = new ObservableCollection<Pokemons>();
+
+                    for (int i = 1; i< 10; i++)
+                    {
+                        MyList.Add(new Pokemons() { Number = i, Name = "piplup" + i.ToString(), Type1 = "water" + i.ToString(), Type2 = "fire" + i.ToString(),Url1 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/393.png" });
+                    }
+
+                    pokemonsList.ItemsSource = MyList;
+        }
+
+       
     }
 }
