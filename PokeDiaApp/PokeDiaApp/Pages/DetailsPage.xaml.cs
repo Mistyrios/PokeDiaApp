92c8fcb0777f48a00ef0459796be80556329b093
Thibault Dufour <thibault.dufour@etu.unilim.fr>
2022-03-02 13:41:12 +0100

    [Feat] detailsPages

--- PokeDiaApp/PokeDiaApp/Pages/DetailsPage.xaml.cs
+++ PokeDiaApp/PokeDiaApp/Pages/DetailsPage.xaml.cs
@@ -9,28 +9,20 @@ namespace PokeDiaApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailsPage : ContentPage
     {
-        public string url;
-        public string Next;
-        public string Previous;
-
+        
         public DetailsPage()
         {
             InitializeComponent();
             BindingContext = this;
 
-            //Pokemons pokemonDeTest = new Pokemons() {
-            //    Description = "test de description",
-            //    Evolution = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/394.png",
-            //    InitialForm = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/393.png",
-            //    Name = "Piplup", Number = 393, Type1 = "Water", Type2 = "fire",
-            //    Url1 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/393.png",
-            //    Url2 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/394.png"
-            //};
-
-            async Task GetPokemon()
-            {
-                HttpClient http = new HttpClient();
-            }
-        }
+            Pokemons pokemonDeTest = new Pokemons() {
+                Description = "test de description",
+                Evolution = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/394.png",
+                InitialForm = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/393.png",
+                Name = "Piplup", Number = 393, Type1 = "Water", Type2 = "fire",
+                Url1 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/393.png",
+                Url2 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/394.png"
+            };
+    }
     }
 }
