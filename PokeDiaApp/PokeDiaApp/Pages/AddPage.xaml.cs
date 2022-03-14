dd7e272c98078f1b6f9ca23cdecda0fc732d3817
Thibault Dufour <thibault.dufour@etu.unilim.fr>
2022-03-14 15:11:20 +0100

    (Fix] conflict

--- PokeDiaApp/PokeDiaApp/Pages/AddPage.xaml.cs
+++ PokeDiaApp/PokeDiaApp/Pages/AddPage.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,22 @@ namespace PokeDiaApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddPage : ContentPage
     {
+        private Pokemon pokemon;
+
         public AddPage()
         {
             InitializeComponent();
+        }
+
+        private void AddButtonClicked(object sender, EventArgs e)
+        {
+            pokemon = new Pokemon();
+            pokemon.Name = name.Text;
+            pokemon.Type1 = FirstType.Text;
+            pokemon.Type2 = SecondType.Text;
+            pokemon.Description = Description.Text;
+            pokemon.Height = Height.Text;
+            pokemon.Weight = Weight.Text;
         }
     }
 }
---
+++ PokeDiaApp/PokeDiaApp/Repository/PokemonRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace PokeDiaApp.Repository
+{
+    public class PokemonRepository
+    {
+        private SQLiteAsyncConnection connection;
+        private Task<Int32> result;
+        public String MessageToShow { get; private set; }
+
+        public PokemonRepository(String DataBasePath)
+        {
+            connection = new SQLiteAsyncConnection(DataBasePath);
+            connection.CreateTableAsync<Pokemon>();
+        }
+
+        public async Task AddPokemon(Pokemon PokemonToAdd)
+        {
+            try {
+                result = connection.InsertAsync(PokemonToAdd);
+                MessageToShow = $"votre pokémon a bien été ajouté";
+            }
+            catch (Exception exception) {
+                MessageToShow = $"votre pokémon n'a pas été ajouté veuillez réessayer \n [Erreur] : {exception.Message}";
+            }
+        }
+
+        public async Task<List<Pokemon>> GetAll()
+        {
+            try {
+                return await connection.Table<Pokemon>().ToListAsync();
+            }
+            catch {
+                MessageToShow = $"[ERREUR] : impossible d'afficher la liste veuillez relancer l'application";
+            }
+
+            return new List<Pokemon>();
+        }
+
+        public async Task RemovePokemon(Pokemon PokemonToRemove)
+        {
+
+        }
+    }
+}
